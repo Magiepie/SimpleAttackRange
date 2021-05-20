75e6e902c6cc71f7b189526f2fb10a377edd0bbe
Magiepie <iamthedustin@gmail.com>
2021-05-20 09:59:31 -0400

    Updates text as you level

--- SimpleAttackRange.lua
+++ SimpleAttackRange.lua
@@ -3,7 +3,23 @@ AttackRange:SetSize(1, 1)
 AttackRange:SetPoint("CENTER", 50, 38)
 local AttackRangeText = AttackRange:CreateFontString(AttackRange, "OVERLAY", "GameTooltipText")
 AttackRangeText:SetPoint("CENTER", 0, 0)
-local SelfLevel  = UnitLevel("Player")
-local AttackLow  = SelfLevel - 3
-local AttackHigh = SelfLevel + 3
-AttackRangeText:SetText("AttackRange: "..AttackLow.."-"..AttackHigh)
+
+function SetAttackRangeText(SelfLevel)
+	local AttackLow  = SelfLevel - 3
+	local AttackHigh = SelfLevel + 3
+	AttackRangeText:SetText("AttackRange: "..AttackLow.."-"..AttackHigh)
+end
+
+SetAttackRangeText(UnitLevel("Player"))
+
+local frame, events = CreateFrame("Frame"), {};
+function events:PLAYER_LEVEL_UP(arg1 )
+	SetAttackRangeText(arg1)
+end
+
+frame:SetScript("OnEvent", function(self, event, ...)
+	events[event](self, ...); 
+end);
+for k, v in pairs(events) do
+	frame:RegisterEvent(k);
+end
